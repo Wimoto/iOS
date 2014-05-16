44dadcb1aeb67bb9fabb98c296b8ef94b949cc7d
eivvanov <e1183i@yandex.ru>
2014-05-16 19:43:08 +0300

    Sensor type definition fixed.

--- code/Wimoto/Shared/Classes/Utils/BLEManager/CBPeripheral+Util.h
+++ code/Wimoto/Shared/Classes/Utils/BLEManager/CBPeripheral+Util.h
@@ -40,7 +40,7 @@ typedef enum {
 #define BLE_GENERIC_CHAR_UUID_SYSTEM_ID                     @"2A23"
 #define BLE_GENERIC_CHAR_UUID_MODEL_NUMBER                  @"2A24"
 
-#define BLE_CLIMATE_MODEL                                   @"Wimoto Climate"
+#define BLE_CLIMATE_MODEL                                   @"Wimoto_Climate"
 #define BLE_CLIMATE_SERVICE_UUID_TEMPERATURE                @"E0035608-EC48-4ED0-9F3B-5419C00A94FD"
 #define BLE_CLIMATE_SERVICE_UUID_LIGHT                      @"E003560E-EC48-4ED0-9F3B-5419C00A94FD"
 #define BLE_CLIMATE_SERVICE_UUID_HUMIDITY                   @"E0035614-EC48-4ED0-9F3B-5419C00A94FD"
@@ -49,14 +49,14 @@ typedef enum {
 #define BLE_CLIMATE_CHAR_UUID_LIGHT_CURRENT                 @"E003560F-EC48-4ED0-9F3B-5419C00A94FD"
 #define BLE_CLIMATE_CHAR_UUID_HUMIDITY_CURRENT              @"E0035615-EC48-4ED0-9F3B-5419C00A94FD"
 
-#define BLE_WATER_MODEL                                     @"Wimoto Water"
+#define BLE_WATER_MODEL                                     @"Wimoto_Water"
 #define BLE_WATER_SERVICE_UUID_PRESENCE                     @"35D8C7DB-9D78-43C2-AB2E-0E48CAC2DBDA"
 #define BLE_WATER_SERVICE_UUID_LEVEL                        @"35D8C7DF-9D78-43C2-AB2E-0E48CAC2DBDA"
 
 #define BLE_WATER_CHAR_UUID_PRESENCE_CURRENT                @"35D8C7DC-9D78-43C2-AB2E-0E48CAC2DBDA"
 #define BLE_WATER_CHAR_UUID_LEVEL_CURRENT                   @"35D8C7E0-9D78-43C2-AB2E-0E48CAC2DBDA"
 
-#define BLE_GROW_MODEL                                      @"Wimoto Grow"
+#define BLE_GROW_MODEL                                      @"Wimoto_Grow"
 #define BLE_GROW_SERVICE_UUID_LIGHT                         @"DAF4470C–BFB0–4DD8–9293–62AF5F545E31"
 #define BLE_GROW_SERVICE_UUID_SOIL_MOISTURE                 @"DAF44712–BFB0–4DD8–9293–62AF5F545E31"
 #define BLE_GROW_SERVICE_UUID_SOIL_TEMPERATURE              @"DAF44706–BFB0–4DD8–9293–62AF5F545E31"
@@ -65,14 +65,14 @@ typedef enum {
 #define BLE_GROW_CHAR_UUID_SOIL_MOISTURE_CURRENT            @"DAF44713–BFB0–4DD8–9293–62AF5F545E31"
 #define BLE_GROW_CHAR_UUID_SOIL_TEMPERATURE_CURRENT         @"DAF44707–BFB0–4DD8–9293–62AF5F545E31"
 
-#define BLE_SENTRY_MODEL                                    @"Wimoto Sentry"
+#define BLE_SENTRY_MODEL                                    @"Wimoto_Sentry"
 #define BLE_SENTRY_SERVICE_UUID_ACCELEROMETER               @"4209DC68-E433–4420-83D8-CDAACCD2E312"
 #define BLE_SENTRY_SERVICE_UUID_PASSIVE_INFRARED            @"4209DC6D-E433–4420-83D8-CDAACCD2E312"
 
 #define BLE_SENTRY_CHAR_UUID_ACCELEROMETER_CURRENT          @"4209DC69-E433–4420-83D8-CDAACCD2E312"
 #define BLE_SENTRY_CHAR_UUID_PASSIVE_INFRARED_CURRENT       @"4209DC6E-E433–4420-83D8-CDAACCD2E312"
 
-#define BLE_THERMO_MODEL                                    @"Wimoto Thermo"
+#define BLE_THERMO_MODEL                                    @"Wimoto_Thermo"
 #define BLE_THERMO_SERVICE_UUID_IR_TEMPERATURE              @"497B8E4E-B61E-4F82-8FE9-B12CF2497338"
 #define BLE_THERMO_SERVICE_UUID_PROBE_TEMPERATURE           @"497B8E54-B61E-4F82-8FE9-B12CF2497338"
 
--- code/Wimoto/Shared/Classes/Utils/BLEManager/CBPeripheral+Util.m
+++ code/Wimoto/Shared/Classes/Utils/BLEManager/CBPeripheral+Util.m
@@ -37,25 +37,20 @@
         if ([aService.UUID isEqual:[CBUUID UUIDWithString:BLE_GENERIC_SERVICE_UUID_DEVICE]]) {
             for (CBCharacteristic *aChar in aService.characteristics) {
                 if ([aChar.UUID isEqual:[CBUUID UUIDWithString:BLE_GENERIC_CHAR_UUID_MODEL_NUMBER]]) {
-                    const uint64_t *byteArray = [aChar.value bytes];
-                    if (byteArray) {
-                        uint64_t value = byteArray[0];
-                        
-                        NSString *model = [NSString stringWithFormat:@"%llu", value];
+                    NSString *model = [[NSString alloc] initWithData:aChar.value encoding:NSUTF8StringEncoding];
 
-                        NSLog(@"peripheralType: MODEL string ------- %@", model);
-                        
-                        if ([model isEqual:BLE_CLIMATE_MODEL]) {
-                            return kPeripheralTypeClimate;
-                        } else if ([model isEqual:BLE_WATER_MODEL]) {
-                            return kPeripheralTypeWater;
-                        } else if ([model isEqual:BLE_GROW_MODEL]) {
-                            return kPeripheralTypeGrow;
-                        } else if ([model isEqual:BLE_SENTRY_MODEL]) {
-                            return kPeripheralTypeSentry;
-                        } else if ([model isEqual:BLE_THERMO_MODEL]) {
-                            return kPeripheralTypeThermo;
-                        }
+                    NSLog(@"peripheralType: MODEL string ------- %@", model);
+                    
+                    if ([model isEqual:BLE_CLIMATE_MODEL]) {
+                        return kPeripheralTypeClimate;
+                    } else if ([model isEqual:BLE_WATER_MODEL]) {
+                        return kPeripheralTypeWater;
+                    } else if ([model isEqual:BLE_GROW_MODEL]) {
+                        return kPeripheralTypeGrow;
+                    } else if ([model isEqual:BLE_SENTRY_MODEL]) {
+                        return kPeripheralTypeSentry;
+                    } else if ([model isEqual:BLE_THERMO_MODEL]) {
+                        return kPeripheralTypeThermo;
                     }
                 }
             }
