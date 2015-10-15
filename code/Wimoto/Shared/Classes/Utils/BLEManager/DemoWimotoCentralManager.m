3926568e3782111f73e7a9847ee71fec3edafc8a
eivvanov <e1183i@yandex.ru>
2015-10-15 11:55:12 +0200

    DemoWimotoCentralManager fixed.

--- code/Wimoto/Shared/Classes/Utils/BLEManager/DemoWimotoCentralManager.m
+++ code/Wimoto/Shared/Classes/Utils/BLEManager/DemoWimotoCentralManager.m
@@ -11,13 +11,13 @@
 @implementation DemoWimotoCentralManager
 
 - (void)startDemoScan {
-    DemoCBPeripheral *climateDemoPeripheral = [[DemoCBPeripheral alloc] init];
-    climateDemoPeripheral.demoPeripheralType = kPeripheralTypeClimateDemo;
-    [(id<DemoWimotoCentralManagerDelegate>)self.wcmDelegate didConnectDemoPeripheral:climateDemoPeripheral];
-    
-    DemoCBPeripheral *thermoDemoPeripheral = [[DemoCBPeripheral alloc] init];
-    climateDemoPeripheral.demoPeripheralType = kPeripheralTypeThermoDemo;
-    [(id<DemoWimotoCentralManagerDelegate>)self.wcmDelegate didConnectDemoPeripheral:thermoDemoPeripheral];
+//    DemoCBPeripheral *climateDemoPeripheral = [[DemoCBPeripheral alloc] init];
+//    climateDemoPeripheral.demoPeripheralType = kPeripheralTypeClimateDemo;
+//    [(id<DemoWimotoCentralManagerDelegate>)self.wcmDelegate didConnectDemoPeripheral:climateDemoPeripheral];
+//    
+//    DemoCBPeripheral *thermoDemoPeripheral = [[DemoCBPeripheral alloc] init];
+//    climateDemoPeripheral.demoPeripheralType = kPeripheralTypeThermoDemo;
+//    [(id<DemoWimotoCentralManagerDelegate>)self.wcmDelegate didConnectDemoPeripheral:thermoDemoPeripheral];
 }
 
 @end
